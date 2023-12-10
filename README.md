d8e5d59e5f14c00c6c1d7171047aad77fbc6fd95
whiteWolfx99 <hhqu180099@uniq.edu.iq>
2023-12-10 19:48:21 +0300

    create BlogList project

---
+++ README.md
@@ -0,0 +1,9 @@
+# Blog List
+
+**BlogList** This is a website dedicated to blog lists where you have the ability to both add and remove your blogs.
+
+## SetUp
+
+You must run the JSON server for the project to function properly.
+
+- npx json-server --watch data/db.json --port 8000
---
+++ src/App.js
@@ -0,0 +1,29 @@
+import Navbar from "./Navbar";
+import Home from "./Home";
+import Create from "./Create";
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import BlogDetails from "./BlogDetails";
+import NotFound from "./NotFound";
+import UpdateBlog from "./UpdateBlog";
+
+function App() {
+  return (
+    <Router>
+      <div className="App">
+        <Navbar />
+        <div className="content">
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/create" element={<Create />} />
+            <Route path="/blogs/:id" element={<BlogDetails />} />
+            <Route path="/update/:id" element={<UpdateBlog />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </div>
+      </div>
+    </Router>
+  );
+}
+
+export default App;
